e9f1aa6f0dc2251a2013b153ea80669829de8ffe
leonrdsa <leonardsalim@gmail.com>
2021-02-02 15:49:06 +0800

    Adding files

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 19:32:17 2021
+
+@author: Lab716A
+"""
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+import pandas as pd
+file = pd.read_excel('Final/Actuals.xlsx')
+
